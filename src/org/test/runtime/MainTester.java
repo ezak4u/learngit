922f8e319a242a140c88183dcbea5b491c7e83b3
ezak4u <ezak4u@gmail.com>
2018-02-23 06:35:28 +0530

    added method first

--- src/org/test/runtime/MainTester.java
+++ src/org/test/runtime/MainTester.java
@@ -3,5 +3,10 @@ package org.test.runtime;
 public class MainTester {
 	public static void main(String[] args) {
 		System.out.println("Tester");
+		
+	}
+	
+	public static void firstMethod() {
+		System.out.println("first Method");
 	}
 }
