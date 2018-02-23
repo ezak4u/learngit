c0063db04baed9d97b8f191bc953cea14042b020
ezak4u <ezak4u@gmail.com>
2018-02-23 06:28:45 +0530

    Remove first method using compare two versions

--- src/org/test/runtime/MainTester.java
+++ src/org/test/runtime/MainTester.java
@@ -3,10 +3,5 @@ package org.test.runtime;
 public class MainTester {
 	public static void main(String[] args) {
 		System.out.println("Tester");
-		
-	}
-	
-	public static void firstMethod() {
-		System.out.println("first Method");
 	}
 }
